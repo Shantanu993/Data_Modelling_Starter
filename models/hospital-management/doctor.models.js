76f835bea8699fc83185de51b379a4f74b4d0f68
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-04 17:02:25 +0530

    complete doctor schema

--- models/hospital-management/doctor.models.js
+++ models/hospital-management/doctor.models.js
@@ -1,5 +1,30 @@
-import mongoose from "mongoose"
+import mongoose, { models } from "mongoose"
 
-const doctorSchema = new mongoose.Schema({},{timestamps: true})
+const doctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    salary: {
+        type: String,
+        required: true
+    },
+    qualification: {
+        type: String,
+        required: true
+    },
+    experienceInYears: {
+        type: Number,
+        default: 0
+    },
+    worksInHospitals: [
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Hospital'
+    },
+],
+
+
+},{timestamps: true})
 
 export const Doctor = mongoose.model("Doctor", doctorSchema)
