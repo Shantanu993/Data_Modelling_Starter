873a4db635ec2f9ebcac43ccb89b418b431ecfb5
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-04 17:11:57 +0530

    complete medical record schema

--- models/hospital-management/medical_record.models.js
+++ models/hospital-management/medical_record.models.js
@@ -1,5 +1,37 @@
 import mongoose from "mongoose"
 
-const medicalRecordSchema = new mongoose.Schema({},{timestamps: true})
+const medicineSchema = new mongoose.Schema({
+    medicineName: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    prescribedFor: {
+        type: String,
+        required: true
+    },
+    frequency: {
+        type: Number,
+        required: true
+    }
+})
+
+const medicalRecordSchema = new mongoose.Schema({
+    name: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Patient',
+        required: true
+    },
+    diagnosedWith: {
+        type: String,
+        required: true
+    },
+    prescribedMedicines: [
+        {medicineSchema}
+    ]
+},{timestamps: true})
 
 export const MedicalRecord = mongoose.model("MedicalRecord", medicalRecordSchema)
